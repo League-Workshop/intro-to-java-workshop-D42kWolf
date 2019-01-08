7a1064138aab1cb815780c83c18cddda6b2762c8
D42kWolf <46504408+D42kWolf@users.noreply.github.com>
2019-01-08 14:30:43 -0800

    Day2

--- src/section2/FourSquare.java
+++ src/section2/FourSquare.java
@@ -6,27 +6,36 @@ import org.jointheleague.graphical.robot.Robot;
 public class FourSquare {
 	
 	// 2. Create a new Robot
-
+Robot shuvi = new Robot();  
 	void go() {
 		// 4. Make the robot move as fast as possible
-
+shuvi.setSpeed(100);
 		// 5. Set the pen width to 5
-
+shuvi.penDown();
+shuvi.setPenWidth(5);
 		// 6. Use a for loop to repeat steps #7 to #8, four times...
-
+for(int i=0; i<10; i++) {
+	
 			// 7. Set the pen color to random
-	
+	shuvi.setRandomPenColor();
 			// 1. Call the drawSquare() method
-	
+	drawSquare(); 
 			// 8. Turn the robot 90 degrees to the right
-
+shuvi.turn(90);}
 	}
 
 	
 	void drawSquare() {
 		JOptionPane.showMessageDialog(null, "yay! you called the drawSquare() method!");
 		/* 3. Fill in the code to draw a square inside the method below. */
-		
+		shuvi.move(100);
+		shuvi.turn(90);
+		shuvi.move(100);
+		shuvi.turn(90);
+		shuvi.move(100);
+		shuvi.turn(90);
+		shuvi.move(100);
+		shuvi.turn(90);
 		
 	}
 
